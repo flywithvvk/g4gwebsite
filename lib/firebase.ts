345a7a0e77a12afe9de6c31a769cd8d25ab2fb62
flywithvvk <flywithvivekraj@gmail.com>
2026-05-03 13:45:06 +0530

    security: remove leaked API keys from repo

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,10 +1,12 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 
+const firebaseApiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? '';
+
 export const firebaseConfig = {
   projectId: 'go4garage-d66fc',
   appId: '1:36448236878:web:24aed293ba5dc7f2c41a1f',
   storageBucket: 'go4garage-d66fc.firebasestorage.app',
-  apiKey: 'AIzaSyDDL2m8f3cTuzlTBWRONpzp2p65qE8WLbw',
+  apiKey: firebaseApiKey,
   authDomain: 'go4garage-d66fc.firebaseapp.com',
   messagingSenderId: '36448236878',
   measurementId: 'G-KJ9Z06N7N5',
